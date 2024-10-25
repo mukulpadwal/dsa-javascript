07d7ba986b77e4fde5ef85f128c32b2c1302042d
mukulpadwal <mukulpadwal.me@gmail.com>
2024-10-25 17:44:34 +0530

    ADD : remove-word method implementation in trie.js class with updated documentation

--- trie/trie.js
+++ trie/trie.js
@@ -3,7 +3,7 @@ class TrieNode {
    * Initializes a new Trie Node with an empty children object and a boolean isEndOfWord that is set to false.
    */
   constructor() {
-    this.children = {};
+    this.children = new Map();
     this.isEndOfWord = false;
   }
 }
@@ -27,11 +27,11 @@ class Trie {
     for (let i = 0; i < word.length; i++) {
       let char = word[i];
 
-      if (!currNode.children[char]) {
-        currNode.children[char] = new TrieNode();
+      if (!currNode.children.has(char)) {
+        currNode.children.set(char, new TrieNode());
       }
 
-      currNode = currNode.children[char];
+      currNode = currNode.children.get(char);
     }
 
     currNode.isEndOfWord = true;
@@ -48,11 +48,11 @@ class Trie {
     for (let i = 0; i < word.length; i++) {
       let char = word[i];
 
-      if (!currNode.children[char]) {
+      if (!currNode.children.has(char)) {
         return false;
       }
 
-      currNode = currNode.children[char];
+      currNode = currNode.children.get(char);
     }
 
     return currNode.isEndOfWord;
@@ -69,14 +69,53 @@ class Trie {
     for (let i = 0; i < word.length; i++) {
       let char = word[i];
 
-      if (!currNode.children[char]) {
+      if (!currNode.children.has(char)) {
         return false;
       }
 
-      currNode = currNode.children[char];
+      currNode = currNode.children.get(char);
     }
 
     return true;
+  }
+
+  removeWord(word) {
+    const deleteRecursively = (node, word, index) => {
+      // Base case: If we reach the end of the word
+      if (index === word.length) {
+        // If the word is found, mark the end of the word as false
+        if (!node.isEndOfWord) {
+          return false; // Word doesn't exist
+        }
+
+        node.isEndOfWord = false; // Unmark the end of the word
+
+        // Return true if the node has no children (can be deleted)
+        return node.children.size === 0;
+      }
+
+      // Get the character at the current index
+      const char = word[index];
+      const childNode = node.children.get(char);
+
+      if (!childNode) {
+        return false; // Word doesn't exist
+      }
+
+      // Recursively delete for the child node
+      const shouldDeleteChild = deleteRecursively(childNode, word, index + 1);
+
+      // If true is returned, delete the mapping for the character
+      if (shouldDeleteChild) {
+        node.children.delete(char);
+        // Return true if the current node should also be deleted
+        return node.children.size === 0 && !node.isEndOfWord;
+      }
+
+      return false; // Don't delete this node
+    };
+
+    deleteRecursively(this.root, word, 0);
   }
 }
 
@@ -85,6 +124,7 @@ class Trie {
  */
 const trie = new Trie();
 trie.insert("apple");
+trie.insert("app");
 trie.insert("banana");
 trie.insert("orange");
 
@@ -99,3 +139,7 @@ console.log(trie.isPrefix("app")); // true
 console.log(trie.isPrefix("ban")); // true
 
 console.log(trie.isPrefix("grape")); // false
+
+trie.removeWord("apple");
+console.log(trie.search("apple")); // false
+console.log(trie.search("app")); // true
--- trie/trie.md
+++ trie/trie.md
@@ -54,3 +54,63 @@ Returns all words in the Trie that start with a given prefix.
 * `Array<string>`: An array of all words in the Trie that start with the given prefix.
 
 ## Example Usage
+
+### 5. `removeWord(word)`
+
+Removes a word from the Trie if it exists.
+
+#### Parameters
+
+* `word`: The word to be removed from the Trie.
+
+#### Returns
+
+* `undefined`
+
+#### Reference
+
+The `removeWord` method is implemented in the `trie.js` file as follows:
+
+```javascript
+removeWord(word) {
+  const deleteRecursively = (node, word, index) => {
+    // Base case: If we reach the end of the word
+    if (index === word.length) {
+      // If the word is found, mark the end of the word as false
+      if (!node.isEndOfWord) {
+        return false; // Word doesn't exist
+      }
+
+      node.isEndOfWord = false; // Unmark the end of the word
+
+      // Return true if the node has no children (can be deleted)
+      return node.children.size === 0;
+    }
+
+    // Get the character at the current index
+    const char = word[index];
+    const childNode = node.children.get(char);
+
+    if (!childNode) {
+      return false; // Word doesn't exist
+    }
+
+    // Recursively delete for the child node
+    const shouldDeleteChild = deleteRecursively(childNode, word, index + 1);
+
+    // If true is returned, delete the mapping for the character
+    if (shouldDeleteChild) {
+      node.children.delete(char);
+      // Return true if the current node should also be deleted
+      return node.children.size === 0 && !node.isEndOfWord;
+    }
+
+    return false; // Don't delete this node
+  };
+
+  deleteRecursively(this.root, word, 0);
+}
+```
+
+
+
